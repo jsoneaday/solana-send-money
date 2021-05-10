35346d82eba075f34aadc8c98695e750096e83c1
David Choi <admin@dzhaven.com>
2021-05-10 18:58:06 -0400

    small refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect } from "react";
 import "./App.css";
-import { initWallet, sendMoney } from "./wallet/wallet";
+import { initWallet, sendMoney } from "./helpers/wallet";
 
 function App() {
   useEffect(() => {
-    initWallet().then(() => sendMoney());
+    initWallet().then(() =>
+      sendMoney("4rqzpkNu7LrFqntX45JTzjtTw35JUvwTb4TYNnE7BbA1")
+    );
   }, []);
 
   return <div className="App"></div>;
--- src/helpers/wallet.ts
+++ src/helpers/wallet.ts
@@ -5,6 +5,7 @@ import {
   SystemProgram,
   Transaction,
   PublicKey,
+  TransactionInstruction,
 } from "@solana/web3.js";
 import EventEmitter from "eventemitter3";
 
@@ -21,15 +22,16 @@ const wallet: WalletAdapter = new Wallet("https://www.sollet.io", network);
 
 export async function initWallet() {
   await wallet.connect();
-  console.log("network", network);
-  console.log("connection", connection);
   console.log("wallet publicKey", wallet?.publicKey?.toBase58());
 }
 
-export async function createChatAccounts() {}
-
-export async function sendMoney(destPubkey: PublicKey) {
+export async function sendMoney(
+  destPubkeyStr: string,
+  lamports: number = 500 * 1000000
+) {
   try {
+    console.log("starting sendMoney");
+    const destPubkey = new PublicKey(destPubkeyStr);
     const walletAccountInfo = await connection.getAccountInfo(
       wallet!.publicKey!
     );
@@ -41,24 +43,37 @@ export async function sendMoney(destPubkey: PublicKey) {
     const instruction = SystemProgram.transfer({
       fromPubkey: wallet!.publicKey!,
       toPubkey: destPubkey,
-      lamports: 500 * 1000000, // about half a SOL
+      lamports, // about half a SOL
     });
-    let trans = new Transaction();
-    trans.add(instruction);
-    trans.feePayer = wallet!.publicKey!;
-    let hash = await connection.getRecentBlockhash();
-    console.log("blockhash", hash);
-    trans.recentBlockhash = hash.blockhash;
+    let trans = await setWalletTransaction(instruction);
 
-    let signedTrans = await wallet.signTransaction(trans);
-    console.log("sign transaction");
-    let signature = await connection.sendRawTransaction(
-      signedTrans.serialize()
-    );
-    console.log("send raw transaction");
+    let signature = await signAndSendTransaction(wallet, trans);
     let result = await connection.confirmTransaction(signature, "singleGossip");
-    console.log("confirm", result);
+    console.log("money sent", result);
   } catch (e) {
     console.warn("Failed", e);
   }
+}
+
+export async function setWalletTransaction(
+  instruction: TransactionInstruction
+): Promise<Transaction> {
+  const transaction = new Transaction();
+  transaction.add(instruction);
+  transaction.feePayer = wallet!.publicKey!;
+  let hash = await connection.getRecentBlockhash();
+  console.log("blockhash", hash);
+  transaction.recentBlockhash = hash.blockhash;
+  return transaction;
+}
+
+export async function signAndSendTransaction(
+  wallet: WalletAdapter,
+  transaction: Transaction
+): Promise<string> {
+  let signedTrans = await wallet.signTransaction(transaction);
+  console.log("sign transaction");
+  let signature = await connection.sendRawTransaction(signedTrans.serialize());
+  console.log("send raw transaction");
+  return signature;
 }
